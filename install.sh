44d2e6597abd01d4b4ccaffef7f604a43bedbc44
Alex Toombs <alex.m.toombs@gmail.com>
2022-02-16 15:39:22 -0800

    Add install.sh script

---
+++ install.sh
@@ -0,0 +1,5 @@
+git clone git@github.com:alextoombs/dotfiles.git
+ln -s ~/dotfiles/.profile ~/.profile
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.zprofile ~/.zprofile
